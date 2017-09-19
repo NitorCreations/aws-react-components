1d4ed3d7a5f378467204c60136f5e1c517888ba1
Richard Weber <richard.weber@nitor.fi>
2017-09-19 10:08:01 +0300

    Cleanup

--- src/lib/LoginForm.tsx
+++ src/lib/LoginForm.tsx
@@ -4,9 +4,6 @@ export namespace LoginForm {
     export interface Props {
         loginHandler: (username: string, password: string) => void;
     }
-    export interface State{
-
-    }
 }
 
 export abstract class LoginForm<P extends LoginForm.Props, S extends {}> extends Component<P, S> {
